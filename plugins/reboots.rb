a022ef52eab7903864144d70f5789b52f91cc8a8
alejandroyoung <alejandro.young@gmail.com>
2014-09-07 01:24:57 +0000

    Created plugin to check for reboots

---
+++ plugins/reboots.rb
@@ -0,0 +1,28 @@
+# Encoding: utf-8
+Ohai.plugin(:Reboots) do
+  provides 'reboots'
+
+  collect_data(:linux) do
+    so = shell_out("last")
+    if so
+      lines = so.stdout.split(/\n/).reject(&:empty?)[2..-1]
+    end
+
+    reboots Array.new
+    if lines
+      for line in lines
+        case line
+	when /reboot/
+	  line_data = line.split()
+	  reboots.push({
+	    :month => line_data[5],
+	    :day => line_data[6],
+	    :time => line_data[7]
+	  })
+	else
+	  next
+        end
+      end 
+    end
+  end
+end
