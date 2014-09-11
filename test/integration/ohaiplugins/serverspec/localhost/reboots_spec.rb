4d308007ad6a3e336893550a3a9df4b205fab562
alejandroyoung <alejandro.young@gmail.com>
2014-09-11 11:34:56 +0000

    Added a spec file

---
+++ test/integration/ohaiplugins/serverspec/localhost/reboots_spec.rb
@@ -0,0 +1,16 @@
+# Encoding: utf-8
+require 'spec_helper'
+
+reboots = OHAI['reboots']
+
+describe 'Reboots Plugin' do
+
+  it 'should be an array' do
+      expect(reboots).to be_a(Array)
+  end
+
+#  it 'should have a value' do
+#    expect(vulnerabilities.keys).not_to be_empty
+#  end
+end
+
